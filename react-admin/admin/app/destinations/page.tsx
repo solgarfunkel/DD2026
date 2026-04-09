271250ce613365ee98b53918b929f39fc93b701b
solgarfunkel <solana.garfunkel1@gmail.com>
2026-04-09 11:10:05 -0400

    assignment and class work

---
+++ react-admin/admin/app/destinations/page.tsx
@@ -0,0 +1,8 @@
+export default function DestinationsPage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold">Destinations Page</h1>
+            <p>This is a list of destinations.</p>
+        </div>
+    );
+}
---
+++ react-admin/admin/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+// basic syntac to create a page in next.js
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Welcome to the Destinations Admin System</h1>
+      <p>This is a simple admin system for managing destinations.</p>
+    </div>
+  );
+}
